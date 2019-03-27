09fc1343be743fb517a5d8d3f85eb0c5042fd871
DaryaTamazina <tamazinatamazina@mail.ru>
2019-03-27 22:42:04 +0300

    labaclicker1.2

--- app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
+++ app/src/main/java/com/example/mobile_task_1_2_clicker/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
-    Button mainBtn;
+    Button mainbtn;
+    Button mainbtn2;
+    Button mainbtn3;
+    ImageView imgbtn;
 
     private long score = 0;
 
@@ -20,19 +24,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainText = (TextView) findViewById(R.id.mainText);
-        mainBtn = (Button) findViewById(R.id.button);
-
+        mainbtn = (Button) findViewById(R.id.button);
+        mainbtn2 = (Button) findViewById(R.id.mainbtn2);
+        mainbtn3 = (Button) findViewById(R.id.mainbtn3);
+        imgbtn = (ImageView) findViewById(R.id.image_view);
 
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                score ++;
-                String s = "Кликов: " + score;
-                mainText.setText(s.toCharArray(),0, s.length());
+                switch (v.getId()){
+                    case R.id.button:
+                        score++;
+                        if(score%10>1 && score%10<5 && score%12!=0 && score%13!=0 && score%14!=0 && score%15!=0){
+                            {
+                                String s = "Кнопка нажата: " + score + " раза";
+                                mainText.setText(s.toCharArray(), 0, s.length());
+                            }}
+                        else {
+                            String ss = "Кнопка нажата: " + score + " раз";
+                            mainText.setText(ss.toCharArray(), 0, ss.length());
+                        }
+                        break;
+                    case R.id.mainbtn2:
+                        if(score>0){
+                            if(score%10>2 && score%10<5 && score%12!=0 && score%13!=0 && score%14!=0 && score%15!=0){
+                                String ss = "Кнопка нажата: " + --score + " раза";
+                                mainText.setText(ss.toCharArray(), 0, ss.length());                                }
+                            else {
+                                String ss = "Кнопка нажата: " + --score + " раз";
+                                mainText.setText(ss.toCharArray(), 0, ss.length());}}
+                        break;
+                    case R.id.mainbtn3:
+                        score=0;
+                        mainText.setText("Кнопка нажата: "+ score + " раз");
+                        break;
+                    case R.id.image_view:
+                        score=0;
+                        mainText.setText("Кнопка нажата: "+ score + " раз");
+                        break;
+                }
             }
         };
 
-        mainBtn.setOnClickListener(clickListener);
+        mainbtn.setOnClickListener(clickListener);
+        mainbtn2.setOnClickListener(clickListener);
+        mainbtn3.setOnClickListener(clickListener);
+        imgbtn.setOnClickListener(clickListener);
 
     }
 }
